e989564f9878ff4dcffc3664ae33ef4afbe484f6
KENT0T <74312360+KENT0T@users.noreply.github.com>
2021-02-01 12:46:31 +0700

    Create banner.py

---
+++ usr/banner.py
@@ -0,0 +1,23 @@
+C = "\033[94;m"
+G = "\033[92;m"
+r = "\033[0;m"
+P = "\033[93;m"
+def banner():
+    print("""
+      ..%%%%...%%%%%....%%%%....%%%%...%%%%%%.
+      .%%..%%..%%..%%..%%..%%..%%..%%....%%...
+      .%%......%%%%%...%%..%%..%%..%%....%%...
+      .%%..%%..%%..%%..%%..%%..%%..%%....%%...
+      ..%%%%...%%..%%...%%%%....%%%%.....%%...
+\tGithub •• https://github.com/Boy-Aja
+      ........................................
+""".format(C,r,G))
+def menu():
+    print(f"1.) Brute Teman")
+    print(f"2.) Brute Like Postingan")
+    print(f"3.) Brute Pencarian Nama")
+    print(f"4.) Brute Grup hanya Bisa Mengambil (100) ID Saja")
+    print(f"5.) Brute Publik")
+    print(f"6.) Brute Tag")
+    print(f"7.) Lihat Hasil")
+
